c50ae213cd9ca7bff1c1e54e60199ed04b7de432
gabri32 <gabodelac@gmail.com>
2025-08-31 14:04:33 -0500

    datros de fix

--- src/app/auth/enviroments.ts
+++ src/app/auth/enviroments.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-apiAuthUrl:'https://backend-inedan.onrender.com/api',
-  apiBaseUrl:'https://backend-inedan.onrender.com/api'
+apiAuthUrl: 'https://backend-inedan-v5q9.onrender.com/api',
+ apiBaseUrl:'https://backend-inedan-v5q9.onrender.com/api'
 
   };
     // apiAuthUrl: 'http://localhost:3525/api',
