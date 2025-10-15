44e5ad576e9c9670e7f585d52a0a4a32b59af61a
gabri32 <gabodelac@gmail.com>
2025-10-14 21:58:22 -0500

    fix tooltip

--- src/app/auth/enviroments.ts
+++ src/app/auth/enviroments.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-apiAuthUrl: 'http://localhost:3525/api',
-apiBaseUrl:'http://localhost:3525/api'
+apiAuthUrl: 'https://backend-inedan-v5q9.onrender.com/api',
+ apiBaseUrl:'https://backend-inedan-v5q9.onrender.com/api'
 
 
   };
